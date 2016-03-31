da49b060f90eaa19b4df86a283fc8365b9886c7c
Norman Peitek <norman.peitek@gmail.com>
2016-03-31 16:14:00 +0200

    added custom okhttp3 downloader example

--- PicassoTutorial/app/src/main/java/io/futurestud/tutorials/picasso/ui/activities/OkHttp3Example.java
+++ PicassoTutorial/app/src/main/java/io/futurestud/tutorials/picasso/ui/activities/OkHttp3Example.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ImageView;
 
-import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.futurestud.tutorials.picasso.R;
+import io.futurestud.tutorials.picasso.okhttp.CustomOkHttp3Downloader;
 
 public class OkHttp3Example extends ActionBarActivity {
 
     @Bind(R.id.standard_list_imageview1) ImageView imageView1;
+    @Bind(R.id.standard_list_imageview2) ImageView imageView2;
 
     private Context context = this;
 
@@ -25,13 +26,14 @@ public class OkHttp3Example extends ActionBarActivity {
         setContentView(R.layout.activity_standard_imageview);
         ButterKnife.bind(this);
 
-        loadImageWithOkHttp3();
+        loadImageWithJakeWhartonsOkHttp3Downloader();
+        loadImageWithCustomOkHttp3Downloader();
     }
 
-    private void loadImageWithOkHttp3() {
+    private void loadImageWithJakeWhartonsOkHttp3Downloader() {
         okhttp3.OkHttpClient client = new okhttp3.OkHttpClient();
         Picasso picasso = new Picasso.Builder(context)
-                .downloader(new OkHttp3Downloader(client))
+                .downloader(new CustomOkHttp3Downloader(client))
                 .build();
 
         // the url could be any image URL, which is accessible with a normal HTTP GET request
@@ -40,6 +42,21 @@ public class OkHttp3Example extends ActionBarActivity {
                 .with(context)
                 .load(internetUrl)
                 .into(imageView1);
+    }
+
+
+    private void loadImageWithCustomOkHttp3Downloader() {
+        okhttp3.OkHttpClient client = new okhttp3.OkHttpClient();
+        Picasso picasso = new Picasso.Builder(context)
+                .downloader(new CustomOkHttp3Downloader(client))
+                .build();
+
+        // the url could be any image URL, which is accessible with a normal HTTP GET request
+        String internetUrl = "http://i.imgur.com/DvpvklR.png";
+        picasso
+                .with(context)
+                .load(internetUrl)
+                .into(imageView2);
     }
 
 }

1309005402a746c142f7167e943d751d45b6294f
Norman Peitek <norman@Normans-MacBook-Pro.local>
2017-04-29 15:11:38 +0200

    fix androidmanifest, add permission request

--- PicassoTutorial/app/src/main/java/io/futurestud/tutorials/picasso/ui/activities/MainActivity.java
+++ PicassoTutorial/app/src/main/java/io/futurestud/tutorials/picasso/ui/activities/MainActivity.java
@@ -1,8 +1,12 @@
 package io.futurestud.tutorials.picasso.ui.activities;
 
+import android.Manifest;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -16,11 +20,24 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // needed for the disk image example
+        requestReadFilePermission();
+
         setListAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 MenuContent.ITEMS));
+    }
+
+    private void requestReadFilePermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    100);
+        }
     }
 
     @Override
